a02e373d50e1312e09ea8b91f055c3ed1da39d44
Claudia Yepes <claudia.yepes0806@hotmail.com>
2023-07-29 22:57:01 -0500

    Filtrar por nombre de anime

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -8,7 +8,7 @@ import { context } from "../Context";
 
 function AppUI(){
     const {searchValue, setSearchValue} = React.useContext(context);
-    
+
 
     // const URLAPI2 = "https://localhost:7205/api/Jikan?q=naruto&page=1";
     // const URLAPI = "https://localhost:7205/api/Jikan";
@@ -32,12 +32,21 @@ function AppUI(){
         
     }, setSearchValue)
 
+      //Estado derivado para filtrar en el array de data
+      const animeFilter = data.filter(
+        (item) =>{
+        const text = item.title.toLowerCase();
+        const searchText = searchValue.toLowerCase();
+        return text.includes(searchText)
+        }
+    )
+
     return(
         <>
             <Header/>
             <SearchAnime/>
             <AnimeList>
-                {data.map((item)=>(
+                {animeFilter.map((item)=>(
                     <AnimeItem key={item.mal_id}
                     src={item.images.jpg.large_image_url}
                     message={item.message}
@@ -45,7 +54,7 @@ function AppUI(){
                 ))}
                 
             </AnimeList>
-            
+
             {/* <Header/>
             <SearchAnime/>
             <AnimeList>
